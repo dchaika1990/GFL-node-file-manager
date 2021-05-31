65cb69cc2c854e88a8679a0fb655c71daf68803d
Dima <dchaika1990@gmail.com>
2021-05-31 08:59:48 +0300

    make object for front

--- public/js/front.js
+++ public/js/front.js
@@ -1,64 +1,3 @@
-const getRequest = async (url) => {
-	let res = await fetch(url)
-	if (!res.ok) throw new Error('Error - ' + res.status);
-	return await res.json();
-}
-
-const postRequest = async (url, key, data) => {
-	const formData = new FormData()
-	formData.append(key, data)
-	return await fetch(url, {
-		method: 'POST',
-		body: formData
-	});
-}
-
-const getCookie = (name) => {
-	let matches = document.cookie.match(new RegExp(
-		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-	))
-	return matches ? decodeURIComponent(matches[1]) : undefined
-}
-
-const displayRows = (input, rows) => {
-	let searchValue = input.value.toLowerCase().trimLeft();
-	rows.forEach(row => {
-		[...row.querySelectorAll('[data-name]')]
-			.some(value => value.textContent.toLowerCase().trimLeft().indexOf(searchValue) > -1)
-			? row.style.display = 'table-row'
-			: row.style.display = 'none'
-	})
-}
-
-const showSearch = (searchInput) => {
-	let rows = document.querySelectorAll('tbody tr');
-	if (searchInput) {
-		if (searchInput.value.toLowerCase().length > 0) displayRows(searchInput, rows);
-		searchInput.addEventListener('keyup', () => {
-			displayRows(searchInput, rows)
-		})
-	}
-}
-
-const makeRender = (selector) => {
-	let template = document.querySelector(selector).innerHTML;
-	return new Function('data', 'return `' + template + '`');
-}
-
-const renderFileSystemMemory = (memoryWrap, memory) => {
-	memoryWrap.innerHTML = '';
-	let listRender = makeRender('.freeSpaceTemplate');
-	memoryWrap.innerHTML = listRender(memory);
-}
-
-const messageAlert = (wrap, message) => {
-	wrap.textContent = message;
-	wrap.classList.add('show');
-	setTimeout(()=> {
-		wrap.classList.remove('show');
-	}, 3500)
-}
-
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 
@@ -73,46 +12,99 @@ document.addEventListener('DOMContentLoaded', () => {
 	let dirUrl = `uploads${separator}${username}`;
 	let dirName = '';
 
-	const renderTableBody = (userFiles, dir = '', up = false) => {
-		const tableBody = table.querySelector('tbody');
-		tableBody.innerHTML = '';
-		let listRender = makeRender('.tableBody');
-		let template = userFiles.map((data) => listRender(data))
-		console.log(dir)
-		if (dir.length
-			&& dir !== `uploads${separator}${username}`
-			// Test on windows
-			// && dir !== `uploads${separator}${username}/`
-		) {
-			let templateDirUp = `
+	function getCookie(name) {
+		let matches = document.cookie.match(new RegExp(
+			"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		))
+		return matches ? decodeURIComponent(matches[1]) : undefined
+	}
+
+	const fileManager = {
+		getRequest: async (url) => {
+			let res = await fetch(url)
+			if (!res.ok) throw new Error('Error - ' + res.status);
+			return await res.json();
+		},
+		postRequest: async (url, key, data) => {
+			const formData = new FormData()
+			formData.append(key, data)
+			return await fetch(url, {
+				method: 'POST',
+				body: formData
+			});
+		},
+		displayRows: (input, rows) => {
+			let searchValue = input.value.toLowerCase().trimLeft();
+			rows.forEach(row => {
+				[...row.querySelectorAll('[data-name]')]
+					.some(value => value.textContent.toLowerCase().trimLeft().indexOf(searchValue) > -1)
+					? row.style.display = 'table-row'
+					: row.style.display = 'none'
+			})
+		},
+		showSearch: (searchInput) => {
+			let rows = document.querySelectorAll('tbody tr');
+			if (searchInput) {
+				if (searchInput.value.toLowerCase().length > 0) fileManager.displayRows(searchInput, rows);
+				searchInput.addEventListener('keyup', () => {
+					fileManager.displayRows(searchInput, rows)
+				})
+			}
+		},
+		makeRender: (selector) => {
+			let template = document.querySelector(selector).innerHTML;
+			return new Function('data', 'return `' + template + '`');
+		},
+		renderFileSystemMemory: (memoryWrap, memory) => {
+			memoryWrap.innerHTML = '';
+			let listRender = fileManager.makeRender('.freeSpaceTemplate');
+			memoryWrap.innerHTML = listRender(memory);
+		},
+		messageAlert: (wrap, message) => {
+			wrap.textContent = message;
+			wrap.classList.add('show');
+			setTimeout(()=> {
+				wrap.classList.remove('show');
+			}, 3500)
+		},
+		renderTableBody: (userFiles, dir = '', up = false) => {
+			const tableBody = table.querySelector('tbody');
+			tableBody.innerHTML = '';
+			let listRender = fileManager.makeRender('.tableBody');
+			let template = userFiles.map((data) => listRender(data))
+			console.log(dir)
+			if (dir.length
+				&& dir !== `uploads${separator}${username}`
+			) {
+				let templateDirUp = `
 				<tr data-up data-dir="${dir}">
 					<td colspan="4">...</td>
 				</tr>
 			`;
-			template = [templateDirUp, ...template]
+				template = [templateDirUp, ...template]
+			}
+			tableBody.innerHTML = (template.join(''))
+			fileManager.showSearch(searchInput);
+		},
+		renderInfo: (options, imgSrc = '', isImg = false) => {
+			const infoUl = document.querySelector('.information-content ul');
+			const infoImg = document.querySelector('.information-media img');
+			const infoDownload = document.querySelector('.information-download');
+			let templateRender = fileManager.makeRender('.infoTemplate')
+			let template = options.map((data) => templateRender(data))
+			infoImg.innerHTML = '';
+			infoDownload.innerHTML = '';
+			infoImg.src = imgSrc
+			if ('true' === isImg) {
+				let downloadLink = document.createElement('a');
+				downloadLink.href = imgSrc
+				downloadLink.download = options[0].value
+				downloadLink.classList.add('btn', 'btn-info')
+				downloadLink.textContent = 'Download'
+				infoDownload.append(downloadLink)
+			}
+			infoUl.innerHTML = template.join('')
 		}
-		tableBody.innerHTML = (template.join(''))
-		showSearch(searchInput);
-	}
-
-	const renderInfo = (options, imgSrc = '', isImg = false) => {
-		const infoUl = document.querySelector('.information-content ul');
-		const infoImg = document.querySelector('.information-media img');
-		const infoDownload = document.querySelector('.information-download');
-		let templateRender = makeRender('.infoTemplate')
-		let template = options.map((data) => templateRender(data))
-		infoImg.innerHTML = '';
-		infoDownload.innerHTML = '';
-		infoImg.src = imgSrc
-		if ('true' === isImg) {
-			let downloadLink = document.createElement('a');
-			downloadLink.href = imgSrc
-			downloadLink.download = options[0].value
-			downloadLink.classList.add('btn', 'btn-info')
-			downloadLink.textContent = 'Download'
-			infoDownload.append(downloadLink)
-		}
-		infoUl.innerHTML = template.join('')
 	}
 
 	if (table) {
@@ -123,22 +115,22 @@ document.addEventListener('DOMContentLoaded', () => {
 				elem = e.target.closest('[data-type="dir"]');
 				dirName = elem.querySelector('[data-name]').textContent;
 				dirUrl = elem.getAttribute('data-dir') + separator + elem.querySelector('[data-name]').textContent
-				getRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`)
+				fileManager.getRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`)
 					.then(({userFiles, memory, parentDir, message}) => {
-					renderTableBody(userFiles, parentDir);
-					renderFileSystemMemory(memoryWrap, memory);
-					messageAlert(messageWrap, message)
-				})
+						fileManager.renderTableBody(userFiles, parentDir);
+						fileManager.renderFileSystemMemory(memoryWrap, memory);
+						fileManager.messageAlert(messageWrap, message)
+					})
 			}
 			if (e.target.closest('[data-up]')) {
 				elem = e.target.closest('[data-up]');
 				let parentUrl = elem.getAttribute('data-dir');
 				dirUrl = parentUrl.slice(0, dirUrl.lastIndexOf(separator))
-				getRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`)
+				fileManager.getRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`)
 					.then(({userFiles, memory, parentDir}) => {
-						renderTableBody(userFiles, parentDir);
-						renderFileSystemMemory(memoryWrap, memory);
-						messageAlert(messageWrap, 'Return back')
+						fileManager.renderTableBody(userFiles, parentDir);
+						fileManager.renderFileSystemMemory(memoryWrap, memory);
+						fileManager.messageAlert(messageWrap, 'Return back')
 					})
 			}
 		})
@@ -172,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
 					path = elem.getAttribute('data-src');
 					isImg = 'true';
 				}
-				renderInfo(options, path, isImg)
+				fileManager.renderInfo(options, path, isImg)
 			}
 		})
 	}
@@ -183,16 +175,16 @@ document.addEventListener('DOMContentLoaded', () => {
 			let input = addFolder.querySelector('input')
 			let nameDir = input.value;
 			if (nameDir.trim().length) {
-				postRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`, 'nameDir', nameDir)
+				fileManager.postRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`, 'nameDir', nameDir)
 					.then( data => data.json())
 					.then( ({userFiles, memory, parentDir, message}) => {
 							input.value = ''
-							renderTableBody(userFiles, parentDir);
-							renderFileSystemMemory(memoryWrap, memory);
-							messageAlert(messageWrap, message)
+							fileManager.renderTableBody(userFiles, parentDir);
+							fileManager.renderFileSystemMemory(memoryWrap, memory);
+							fileManager.messageAlert(messageWrap, message)
 					})
 					.catch(error => {
-						messageAlert(messageWrap, error)
+						fileManager.messageAlert(messageWrap, error)
 					})
 			}
 		})
@@ -204,20 +196,20 @@ document.addEventListener('DOMContentLoaded', () => {
 			let input = formUpload.querySelector('[type="file"]')
 			let file = input.files[0]
 			if (file) {
-				postRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`, 'myFile', file)
+				fileManager.postRequest(`http://localhost:3010/${username}-files/?folderUrl=${dirUrl}`, 'myFile', file)
 					.then(response => response.json())
 					.then(({userFiles, memory, parentDir, message}) => {
 						input.value = ''
-						renderTableBody(userFiles, parentDir);
-						renderFileSystemMemory(memoryWrap, memory);
-						messageAlert(messageWrap, message)
+						fileManager.renderTableBody(userFiles, parentDir);
+						fileManager.renderFileSystemMemory(memoryWrap, memory);
+						fileManager.messageAlert(messageWrap, message)
 					})
 					.catch(error => {
-						messageAlert(messageWrap, error)
+						fileManager.messageAlert(messageWrap, error)
 					})
 			}
 		})
 	}
 
-	showSearch(searchInput);
+	fileManager.showSearch(searchInput);
 })
